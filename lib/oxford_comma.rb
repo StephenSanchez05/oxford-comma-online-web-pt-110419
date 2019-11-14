2cebb49f3759db19fa52c5268eacea643ebb92e3
Stephen Sanchez <stephensanchez05@gmail.com>
2019-11-14 02:52:58 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.size == 2
+    array.join(" and ")
+  elsif array.size > 2
+  last_fruit = array.pop
+  five_array = array.join(", ")
+  five_array << ", and #{last_fruit}"
+else
+ array.join
+end
 end
